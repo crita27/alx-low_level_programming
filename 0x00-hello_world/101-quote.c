0a359a19179c40475dba72f0b95018e6ea52a010
Mesfert <critaswe@gmail.com>
2023-10-05 09:58:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - this prints exactly the input on it 
- * Return: always 1 (Success)
+ * main - A c program tht prints a line to the standard errors
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
