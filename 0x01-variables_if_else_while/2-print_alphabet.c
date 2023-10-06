0684f816dee2829254dd39418fbdbf84b7008962
Mesfert <critaswe@gmail.com>
2023-10-06 01:13:29 -0700

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char m;
+for (m = 'a' ; m <= 'z' ; m++)
+putchar(m);
+putchar('\n');
+return (0);
+}
