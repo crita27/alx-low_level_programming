50a9f24fa01875f8387548406024f3d2d9fa2ea6
Mesfert <critaswe@gmail.com>
2023-10-06 01:36:51 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int m;
+for (m = 0 ; m < 10 ; m++)
+putchar(m);
+putchar('\n');
+return (0);
+}
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char m;
+for (m = 'z' ; m >= 'a' ; m--)
+putchar(m);
+putchar('\n');
+return (0);
+}
