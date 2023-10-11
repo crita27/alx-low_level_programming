7136ba699203146c15ffa5c31cf71399c504322a
Mesfert <critaswe@gmail.com>
2023-10-11 00:01:47 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -ten times
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char m;
+for (i = 1 ; i <= 10 ; i++)
+{
+for (m = 'a' ; m <= 'z' ; m++)
+{
+_putchar('m');
+_putchar('\n');
+}
