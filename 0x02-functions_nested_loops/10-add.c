2b3402221e380a523c8a7c917818ef4fee349b38
Mesfert <critaswe@gmail.com>
2023-10-11 18:33:11 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - aff numbers
+ * Return: 0
+ * @i: number to be add
+ * @j: other number to be add
+ */
+int add (int i, int j)
+{
+	return (i + j);
+}
