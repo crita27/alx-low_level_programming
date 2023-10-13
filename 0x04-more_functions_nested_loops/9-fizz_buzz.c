1e77568e55e5249673e3cd75317e7fe15ca6347e
Mesfert <critaswe@gmail.com>
2023-10-13 05:35:38 -0700

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print 1 to 100
+ * @
+ * Return: list
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d", i);
+printf(" ");
+}
+return (0);
+}
