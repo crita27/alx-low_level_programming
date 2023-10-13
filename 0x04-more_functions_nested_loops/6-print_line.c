19755df74a2c9f44c174872c5008be6ad2f7b64e
Mesfert <critaswe@gmail.com>
2023-10-12 20:03:42 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line = draws straightline
+ * @n: number of times 
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int j;
+for (j = 1; j <= n; j++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
+
