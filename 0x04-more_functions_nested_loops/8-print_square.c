34f300d7f2f0f059b7db1796c2a8d62f95722e4a
Mesfert <critaswe@gmail.com>
2023-10-13 04:33:47 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,12 @@
 void print_square(int size)
 {
 int i;
+if (size < 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 1; i <= size; i++)
 {
 int j;
@@ -17,5 +23,6 @@ for (j = 1; j <= size; j++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
