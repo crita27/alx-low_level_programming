df3fe8567e4cdb342c09887da41e6b5e61388cab
Mesfert <critaswe@gmail.com>
2023-10-17 18:14:33 -0700

    list

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2 - print one character
+ * @str: input
+ * return: 0
+ */
+void puts2(char *str)
+{
+int i = 0;
+int t = 0;
+while (*str != '\0')
+{
+str++;
+i++;
+}
+t  i - 1;
+int j
+for (j = 0; j <= t; j++)
+{
+if (j % 2 == 0)
+{
+putchar(str[j]);
+}
+}
+putchar('\n');
+}
