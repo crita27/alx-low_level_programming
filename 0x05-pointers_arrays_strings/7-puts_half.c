5764f3fd1ac4b60a27bd1a3726e55d769c943e3d
Mesfert <critaswe@gmail.com>
2023-10-17 18:36:38 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@
 void puts_half(char *str)
 {
 int i, m, l;
-longi = 0;
+l = 0;
 for (i = 0; str[i] != '\0'; i++)
 l++;
 m = (l / 2);
 if ((l % 2) == 1)
-n = ((l + 1) / 2);
+m = ((l + 1) / 2);
 for (i = m; str[i] != '\0'; i++)
 putchar(str[i]);
 putchar('\n');
