f1c32ce4439ae24455eb927c0ed9a2abf6bd941e
Mesfert <critaswe@gmail.com>
2023-10-27 19:10:31 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- *  * main - print name
- *   * @argc: argument count
- *    * @argv: argument vector
- *     *
- *      * Return: always 0
- *       */
+ * main - print name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	        (void) argc;
-		        printf("%s\n", argv[0]);
-
-			        return (0);
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
