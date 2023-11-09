4f376833ab09e4fa91b677d51cdec3bb81293926
Mesfert <critaswe@gmail.com>
2023-11-08 17:23:30 -0800

    0x0f

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: execute function
+ * @size: size of array
+ * @action: pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
