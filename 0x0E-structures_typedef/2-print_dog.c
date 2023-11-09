43c2133613fe76316999f288fb9a5e66c6d47b24
Mesfert <critaswe@gmail.com>
2023-11-08 18:07:08 -0800

    0x0e

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct dog {
+    char *name;
+    float age;
+    char *breed;
+};
+
+void print_dog(struct dog *d)
+{
+    if (d == NULL)
+        return;
+
+    printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+    printf("Age: %.1f\n", d->age);
+    printf("Breed: %s\n", d->breed != NULL ? d->breed : "(nil)");
+}
