df7ce15b45965ae3c42d098c8ecb213d574b1b32
Mesfert <critaswe@gmail.com>
2023-11-20 08:54:43 -0800

    0-print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * size_t print_listint - print all elements
+ * @h: list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *m = h;
+	size_t count = 0;
+
+	while (m != NULL)
+	{
+		printf("%d\n", m->n);
+		count++;
+		m = m->n;
+	}
+	return (count);
+}
