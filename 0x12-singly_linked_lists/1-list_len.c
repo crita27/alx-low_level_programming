d604a9e1511f073838aa5163e369673c404031f8
Mesfert <critaswe@gmail.com>
2023-11-21 00:11:00 -0800

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_lem - return number of eleements
+ * @h: pointer
+ * Return: number of element
+ */
+size_t list_len(const list_t *h)
+{
+	size_t m = 0;
+
+	while(h)
+	{
+		m++;
+		h = h->next;
+	}
+	return (m);
+}
