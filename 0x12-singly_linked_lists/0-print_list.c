21c4ed2744d648ec2e8f99e294e7306809ae6561
Mesfert <critaswe@gmail.com>
2023-11-21 00:03:40 -0800

    0 print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print elements
+ * @h: pointer
+ * Return: number of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t m = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h=h->next;
+		m++;
+	}
+	return(m);
+}
